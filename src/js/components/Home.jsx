fd7724701ca40b549f453a839f445e198635554e
Marcos <111812112+Marcos33330@users.noreply.github.com>
2026-05-23 01:24:40 +0000

    primer paso listo, aun falta

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+	const [tasks, setTasks] = useState([]);
+	const [inputValue, setInputValue] = useState("");
+
+	function addTask(e) {
+
+		if (e.key === "Enter" && inputValue !== "") {
+
+			fetch('https://playground.4geeks.com/todo/todos/Marcos', {
+				method: 'POST', // or 'POST'
+				body: JSON.stringify({
+					"label": inputValue,
+					"is_done": false
+				}), // data can be a 'string' or an {object} which comes from somewhere further above in our application
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			})
+				.then(res => {
+					if (!res.ok) throw Error(res.statusText);
+					return res.json();
+				})
+				.then(response => console.log('Success:', response))
+				.catch(error => console.error(error));
+
+			// setTasks([...tasks, inputValue]);
+			// setInputValue("");
+
+		}
+	}
+
+	function getTask() {
+		fetch('https://playground.4geeks.com/todo/users/Marcos', {
+			method: 'GET'
+		})
+
+
+			.then((response) => {
+				return response.json();
+			})
+
+
+			.then((data) => { console.log(data) })
+
+
+			.catch(() => { })
+
+
+
+	}
+
+	function deleteTask(index) {
+
+		let newTasks = tasks.filter(function (task, i) {
+
+			return i !== index;
+		});
+
+		setTasks(newTasks);
+	}
+
 	return (
-		<div className="text-center">
-            
+		<div className="container mt-5 d-flex justify-content-center">
+			<button onClick={() => { getTask() }}>
+				<p>prueba</p>
+			</button>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+
+
+
 		</div>
-	);
-};
 
+
+
+
+	)
+}
 export default Home;
